dc38752c8c4c3b993de8f298168acb39eb01a4e9
pinondangs21 <pinondangs21@gmail.com>
2024-02-27 13:58:46 +0700

    first-commit

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Product } from '../interfaces/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  constructor() {}
+
+  createDb() {
+    const products = [
+      { id: 1, name: 'Laptop', stock: 20, price: 7000000, photo: 'img' },
+      { id: 2, name: 'Televisi', stock: 20, price: 5000000, photo: 'img' },
+    ];
+
+    return { products };
+  }
+
+  genId(products: Product[]): number {
+    return products.length > 0
+      ? Math.max(...products.map((c) => c.id)) + 1
+      : 11;
+  }
+}
